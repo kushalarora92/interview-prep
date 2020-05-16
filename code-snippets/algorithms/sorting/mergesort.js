3422c15e71c1da08e726af825770ac68c2dc1635
Kushal Arora <kushalarora92@gmail.com>
2020-05-17 00:34:04 +0530

    added merge sort

---
+++ code-snippets/algorithms/sorting/mergesort.js
@@ -0,0 +1,35 @@
+export default function mergesort(arr) {
+  // find mid & split arrays by mid
+  // repeat above step until arr.length <=1
+
+  if (arr.length <= 1) return arr;
+
+  const mid = Math.floor(arr.length / 2);
+  
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+
+  return merge(
+    mergesort(left),
+    mergesort(right)
+  );
+}
+
+function merge(left, right) {
+  const result = [];
+  let leftIndex = 0, rightIndex = 0;
+
+  while(leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+
+  return result
+    .concat(left.slice(leftIndex))
+    .concat(right.slice(rightIndex));
+}
